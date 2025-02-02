26545bf17143f1d20c6fbcb9671a430e1b5a4cd8
Jefflux777 <sicajau.jefferson.ipcs@gmail.com>
2025-02-01 23:55:54 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const title = document.querySelector('.title')
+const text = 'HOLA!DANNITA'.split('')
+const text = 'De parte de yeff ¿QUIERES SER MI SAN VALENTIN?<3'.split('')
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}<span/>`
+  } else {
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+  }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  element.style.animationDelay = `${randomDelay}s`;
+});
