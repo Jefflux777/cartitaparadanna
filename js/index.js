a777fee4d267decc2c98fcebe523a6f99dda2a93
Jefflux777 <sicajau.jefferson.ipcs@gmail.com>
2025-02-02 00:06:20 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'De parte de yeff ¿QUIERES SER MI SAN VALENTIN?<3'.split('')
+const text = 'I Have Something'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
