571a9256c86d862fe0474ae0567b8c40dcb2b888
Jefflux777 <sicajau.jefferson.ipcs@gmail.com>
2025-02-04 16:03:09 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
 const title = document.querySelector('.title')
-const text = 'OLA!DANNITA'.split('')
-for (let index = 0; index < text.length; index++) {
-  if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
-  } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
-  }
-}
 const text = 'De parte de yeff ¿QUIERES SER MI SAN VALENTIN<3'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
